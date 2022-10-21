72a138ce294f3b1e3c8cfe97a59aecbfee615604
trankhai1478 <trankhai1478@gmail.com>
2022-10-21 09:40:33 +0700

    86

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,20 +1,115 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuizId } from "../../Service/ApiService";
+import _ from 'lodash';
+import './DetailQuiz.scss';
+import Question from "./Question";
+
 const DetailQuiz = (props) => {
     const param = useParams();
+    const location = useLocation();
+
     const quizId = param.id;
+    const [dataQuiz, setDataQuiz] = useState([]);
+    const [index, setIndex] = useState(0);
+
     useEffect(() => {
         fecthQuestions();
     }, [quizId]);
     const fecthQuestions = async () => {
         let res = await getDataQuizId(quizId);
         console.log("check question", res)
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription = null;
+                    let image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        item.answers.isSelected = false;
+                        answers.push(item.answers)
+
+                    })
+
+                    //answers.questionId = key;
+                    return { questionId: key, answers, questionDescription, image }
+                }
+                )
+                .value();
+            setDataQuiz(data);
+
+
+        }
     }
-    console.log("check param", param)
+    const handlePrev = () => {
+        if (index - 1 < 0) return;
+        setIndex(index - 1);
+    }
+    const handleNext = () => {
+        if (dataQuiz && dataQuiz.length > index + 1) {
+            setIndex(index + 1);
+        }
+
+    }
+    const handleCheckBox = (answerId, questionId) => {
+        let dataQuizClone = _.cloneDeep(dataQuiz);
+        let question = dataQuizClone.find(item => +item.questionId === +questionId);
+        if (question && question.answers) {
+            question.answers = question.answers.map(item => {
+                if (item.id === +answerId) {
+                    item.isSelected = !item.isSelected;
+                }
+                return item;
+            })
+
+            //console.log("check b", b);
+        }
+        let index = dataQuizClone.findIndex(item => +item.questionId === +questionId);
+        if (index > -1) {
+            dataQuizClone[index] = question;
+            setDataQuiz(dataQuizClone);
+        }
+    }
     return (
         <div className="detail-quiz-container">
-            hahaha
+            <div className="left-content">
+                <div className="title">
+                    Quiz {quizId} :{location?.state?.quizTitle}
+                </div>
+                <hr />
+                <div className="q-body">
+                    <img />
+                </div>
+                <div className="q-content">
+                    <Question
+                        index={index}
+                        handleCheckBox={handleCheckBox}
+                        data={dataQuiz && dataQuiz.length > 0
+                            ?
+                            dataQuiz[index]
+                            :
+                            []}
+                    />
+                </div>
+                <div className="footer">
+
+                    <button className="btn btn-secondary"
+                        onClick={() => handlePrev()}>Prev</button>
+                    <button className="btn btn-primary"
+                        onClick={() => handleNext()}>Next</button>
+                </div>
+            </div>
+            <div className="right-content">
+                count down
+            </div>
         </div>
     )
 }
---
+++ src/components/User/Question.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+const Question = (props) => {
+    const { data, index } = props;
+    if (_.isEmpty(data)) {
+        return (<></>)
+    }
+    const handleCheckBox = (event, aId, qId) => {
+
+        console.log('props', aId, qId);
+        props.handleCheckBox(aId, qId)
+    }
+    return (
+        <>
+            {data.image &&
+                <div className='q-image'>
+                    <img src={`data:image/jpeg;base64,${data.image}`} />
+                </div>
+            }
+            <div className="question">
+                Question {index + 1} : {data.questionDescription} ?
+            </div>
+            <div className="answer">
+                {data.answers && data.answers.length &&
+                    data.answers.map((a, index) => {
+                        return (
+                            <div
+                                key={`answer-${index}`}
+                                className="a-child">
+                                <div className="form-check">
+                                    <input class="form-check-input"
+                                        type="checkbox"
+                                        checked={a.isSelected}
+                                        onChange={(event) => handleCheckBox(event, a.id, data.questionId)}
+                                    />
+                                    <label className="form-check-label">
+                                        {a.description}
+                                    </label>
+                                </div>
+
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+        </>
+    )
+}
+export default Question;
